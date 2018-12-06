397216e4a35c0247d209ae7f662ce5c3b2e7e8aa
Andrew Park <13parksangjoon@gmail.com>
2018-12-06 15:19:44 -0800

    heroku key update

--- src/components/aural-status.test.js
+++ src/components/aural-status.test.js
@@ -9,5 +9,11 @@ describe('<AuralStatus />', function() {
         shallow(<AuralStatus />);
     });
 
+    it('Should have the screenreader component', function() {
+        const wrapper = shallow(<AuralStatus />);
+        expect(wrapper.hasClass('visuallyhidden')).to.equal(true);
+    });
+
+    
 
 });
---
+++ src/components/feedback.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {shallow, mount} from 'enzyme';
+import Feedback from './feedback.js';
+
+describe('<Feedback />', function() {
+    it('Renders without crashing', function() {
+        shallow(<Feedback />);
+    });
+
+    
+
+});
