ccea0d83fd94bdc72f2e604220254283ae0b3789
Andrew Park <13parksangjoon@gmail.com>
2018-12-05 18:51:32 -0800

    added static.json for heroku deployment

--- src/components/aural-status.test.js
+++ src/components/aural-status.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {shallow, mount} from 'enzyme';
 
 import AuralStatus from './aural-status.js';
-import { isIterable } from 'core-js';
 
 Describe('<AuralStatus />', function() {
     it('Renders without crashing', function() {
